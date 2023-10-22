b637531a468cb198831396490989cec5271a1216
Bhoj Raj Mishra <bhojrajmishra4@gmail.com>
2023-10-22 20:41:04 +0545

    raw-node server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req,res)=>{
-console.log(req);
+const server = http.createServer(routes.handler);
+console.log(routes.someText);
+
+server.listen(4000,function(){
+    console.log("NodeJs project has started in 4000")
 });
-
-server.listen(4000);
